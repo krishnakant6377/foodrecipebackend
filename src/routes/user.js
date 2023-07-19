43c9fb583fe4f9bf6ca89097d6504002e9adaa4c
Krishnakant Verma <111070484+krishnakant6377@users.noreply.github.com>
2023-07-19 15:33:30 +0530

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const userModel = require("../models/Users")
 
 const router = express.Router()
